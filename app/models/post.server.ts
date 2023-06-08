c623149ae56a884992c0e022f5603c3b3768e4fb
nickherrig <neherrig@gmail.com>
2023-06-08 13:18:49 -0500

    Complete createPost() model, and route action with validation and useActionData

--- app/models/post.server.ts
+++ app/models/post.server.ts
@@ -1,6 +1,5 @@
 import supabase from "~/utils/supabase.server"
 
-
 export async function getPosts() {
     
     const { data: posts, error} = await supabase
@@ -11,10 +10,25 @@ export async function getPosts() {
 }
 
 export async function getPost(slug: string) {
+    
     const {data: post, error } = await supabase
         .from('posts')
         .select("*")
         .eq('slug', slug)
 
     return post
+}
+
+export async function createPost(slug: string, title: string, markdown: string) {
+
+    const { data, error } = await supabase
+        .from('posts')
+        .insert([
+            {   slug: slug, 
+                title: title,
+                markdown:markdown, 
+            },
+        ])
+    
+    return data
 }
